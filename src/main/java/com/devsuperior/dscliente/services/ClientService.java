d1ae6b1e8dde3a4c3823d51fa03a2ff3dbb9e86d
rafaelspa <rsdepaula67@gmail.com>
2022-12-28 15:54:26 -0300

    Resource not found exception

--- src/main/java/com/devsuperior/dscliente/services/ClientService.java
+++ src/main/java/com/devsuperior/dscliente/services/ClientService.java
@@ -3,7 +3,10 @@ package com.devsuperior.dscliente.services;
 import com.devsuperior.dscliente.dto.ClientDto;
 import com.devsuperior.dscliente.entities.Client;
 import com.devsuperior.dscliente.repositories.ClientRepository;
+import com.devsuperior.dscliente.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -23,7 +26,9 @@ public class ClientService {
 
     @Transactional(readOnly = true)
     public ClientDto findById(Long id) {
-        Client client = clientRepository.findById(id).get();
+        Client client = clientRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Resource not found")
+        );
         return clientToClientDto(client);
     }
 
@@ -42,19 +47,29 @@ public class ClientService {
 
     @Transactional
     public ClientDto update(Long id, ClientDto dto) {
-        Client client = clientRepository.getReferenceById(id);
-        client.setName(dto.getName());
-        client.setCpf(dto.getCpf());
-        client.setIncome(dto.getIncome());
-        client.setBirthDate(dto.getBirthDate());
-        client.setChildren(dto.getChildren());
-        client = clientRepository.save(client);
-        return clientToClientDto(client);
+        try {
+            Client client = clientRepository.getReferenceById(id);
+            client.setName(dto.getName());
+            client.setCpf(dto.getCpf());
+            client.setIncome(dto.getIncome());
+            client.setBirthDate(dto.getBirthDate());
+            client.setChildren(dto.getChildren());
+            client = clientRepository.save(client);
+            return clientToClientDto(client);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Resource not found");
+        }
     }
 
     @Transactional
     public void delete(Long id) {
-        clientRepository.deleteById(id);
+        try {
+            clientRepository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Resource not found");
+        }
     }
 
     public ClientDto clientToClientDto(Client client) {
