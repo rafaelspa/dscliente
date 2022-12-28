1ca4f1e45511cb482af39bcf286cf09cb4ecbe2c
rafaelspa <rsdepaula67@gmail.com>
2022-12-28 14:05:47 -0300

    Client Dto

---
+++ src/main/java/com/devsuperior/dscliente/dto/ClientDto.java
@@ -0,0 +1,51 @@
+package com.devsuperior.dscliente.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+public class ClientDto {
+
+    private Long id;
+    @NotBlank(message = "Name can not be blank")
+    private String name;
+    private String cpf;
+    private String income;
+    @PastOrPresent(message = "Birth date can not be in the future")
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDto(Long id, String name, String cpf, String income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getIncome() {
+        return income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+}
